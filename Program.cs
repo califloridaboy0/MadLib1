52f1e3bff58e919e91ac4dbbab7f8bab2140093b
califloridaboy0 <79368149+califloridaboy0@users.noreply.github.com>
2021-08-04 21:50:10 -0400

    Madlib attempt 1 in C#

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MadLib1
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a verb (hit Enter)");
+            string verb1 = Console.ReadLine();
+
+            Console.WriteLine("Enter another verb");
+            string verb2 = Console.ReadLine();
+
+            Console.WriteLine("Enter a place");
+            string place1 = Console.ReadLine();
+
+            Console.WriteLine("Enter an adverb");
+            string adv1 = Console.ReadLine();
+
+            Console.WriteLine("Enter a celebrity's name");
+            string celeb1 = Console.ReadLine();
+
+            string NewVerb = Verb.SentBuilder1();
+
+            string NewFrag = Verb.SentBuilder2();
+
+            Console.WriteLine(celeb1+ NewVerb + adv1 + " " + verb1 + NewFrag + place1+". ");
+        }
+    }
+}
---
+++ Verb.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace MadLib1
+{
+    public class Verb
+    {
+
+        static public string SentBuilder1()
+        {
+            Console.WriteLine("Enter a number: 1, 2, 3, or 4.");
+            int x = Convert.ToInt16(Console.ReadLine());
+
+            switch (x) {
+                case 1: return (" will ");
+                    break;
+                case 2: return (" wants to ");
+                    break;
+                case 3: return (" will never ");
+                    break;
+                case 4: return (" is dying to ");
+                default: return (" Error ");
+            }
+        }
+
+        static public string SentBuilder2()
+        {
+            Console.WriteLine("Enter another number: 1, 2, or 3.");
+            int y = Convert.ToInt16(Console.ReadLine());
+            switch (y)
+            {
+                case 1: return (" all the way to ");
+                    break;
+                case 2: return (" all the way back from ");
+                    break;
+                case 3: return (" while at ");
+                    break;
+                default: return("Error");
+            }
+        }
+
+        public Verb()
+        {
+            //public string[] NewVerb = new string[3];
+            
+        }
+    }
+}
